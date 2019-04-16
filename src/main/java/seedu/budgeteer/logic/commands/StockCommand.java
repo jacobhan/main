f2824358c625243e611bc32bf73d88d7cb7e460e
jacobhan <jacobhan@Jacobs-MacBook-Pro.local>
2019-04-16 14:16:57 +0800

    added credit

--- src/main/java/seedu/budgeteer/logic/commands/StockCommand.java
+++ src/main/java/seedu/budgeteer/logic/commands/StockCommand.java
@@ -32,8 +32,8 @@ public class StockCommand extends Command {
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "MSFT";
 
-    private String firstUrl = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=";
-    private String secondUrl = "&apikey=Y6G36I3BIPQL5I2";
+    private String firstUrl = "https://api.iextrading.com/1.0/stock/";
+    private String secondUrl = "/price";
 
     private final Name name;
 
@@ -48,6 +48,7 @@ public class StockCommand extends Command {
         String ret = "";
         try {
             String temp = firstUrl + name.fullName.toUpperCase() + secondUrl;
+            System.out.println(temp);
 
             URL url = new URL(temp);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -61,13 +62,7 @@ public class StockCommand extends Command {
 
             BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
 
-            String output;
-            int lineNumber = 7;
-            while (lineNumber > 0) {
-                output = br.readLine();
-                ret = output;
-                lineNumber -= 1;
-            }
+            ret = br.readLine();
 
             conn.disconnect();
 
@@ -93,10 +88,8 @@ public class StockCommand extends Command {
 
         if (full == null) {
             messageReturn = "Sorry, your input is not a valid stock. Please try again.";
-        } else if (full.length() < 30) {
-            messageReturn = "Sorry, your input is not a valid stock. Please try again.";
         } else {
-            price = Float.parseFloat(full.substring(22, 28));
+            price = Float.parseFloat(full);
             Double printPrice = (double) Math.round(price * 100.0) / 100.0;
 
             String first = "You are able to buy ";
